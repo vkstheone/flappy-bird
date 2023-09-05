a1cc78debe736fa6876fb46655c1add1769b9414
vkstheone <89942870+vkstheone@users.noreply.github.com>
2023-09-05 18:43:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,30 +3,43 @@ function novoElemento(tagName, className) {
     elem.className = className
     return elem
 }
-function Barreira(reversa = false) { 
-  this.elemento = novoElemento('div','barreira')
+function Barreira(reversa = false) {
+    this.elemento = novoElemento('div', 'barreira')
 
-  const borda = novoElemento('div', 'borda')
-  const corpo = novoElemento('div', 'corpo')
-  this.elemento.appendChild(reversa ? corpo : borda)
-  this.elemento.appendChild(reversa ? borda : corpo)
+    const borda = novoElemento('div', 'borda')
+    const corpo = novoElemento('div', 'corpo')
+    this.elemento.appendChild(reversa ? corpo : borda)
+    this.elemento.appendChild(reversa ? borda : corpo)
 
-  this.SetAltura = altura => corpo.style.height  = `${altura}px`
+    this.setAltura = altura => corpo.style.height = `${altura}px`
 }
-//const b = new Barreira(true)
-// b.setAltura(300)
-//document.querySelector('[wm-flappy]').appendChild
+
+// const b = new Barreira(false)
+// b.setAltura(400)
+// document.querySelector('[wm-flappy]').appendChild(b.elemento)
 
 function ParDeBarreiras(altura, abertura, x) {
-  this.elemento = novoElemento('div','par-de-barreiras')
+    this.elemento = novoElemento('div', 'par-de-barreiras')
 
-  this.superior = new Barreira(true)
-  this.inferior = new Bareiras(false)
+    this.superior = new Barreira(true)
+    this.inferior = new Barreira(false)
 
-  this.elemento.appendChild(this.superior.elemento)
-  this.elemento.appendChild(this.inferior.elemento)
+    this.elemento.appendChild(this.superior.elemento)
+    this.elemento.appendChild(this.inferior.elemento)
 
-  this.sortearAbertura = () = >{
-    
-  }
+    this.sortearAbertura = () => {
+        const alturaSuperior = Math.random() * (altura - abertura)
+        const alturaInferior = altura - abertura - alturaSuperior
+        this.superior.setAltura(alturaSuperior)
+        this.inferior.SetAltura(alturaInferior)
+
+    }
+    this.get = () => parseInt(this.elemento.style.left.split('px')[0])
+    this.setX = x => this.elemento.style.left = `${x}px`
+    this.getLargura = () => this.elemento.clientWidth
+
+    this.sortearAbertura()
+    this.setX(x)
 }
+const b = new ParDeBarreiras(800, 200, 400)
+document.querySelector('[wm-flappy]').appendChild(b.elemento)
